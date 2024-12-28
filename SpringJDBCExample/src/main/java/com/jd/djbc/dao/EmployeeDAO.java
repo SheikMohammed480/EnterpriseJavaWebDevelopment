06d18efdd4afd4022845f353263378cd1368641b
SheikMohammed480 <sheikmohammed480@gmail.com>
2024-12-28 16:02:40 +0530

    Add files via upload

---
+++ SpringJDBCExample/src/main/java/com/jd/djbc/dao/EmployeeDAO.java
@@ -0,0 +1,22 @@
+package com.jd.djbc.dao;
+import java.util.List;
+
+
+
+import com.jd.djbc.model.Employee;
+
+
+//CRUD operations
+public interface EmployeeDAO {
+	
+	//Create
+	public void save(Employee employee);
+	//Read
+	public Employee getById(int id);
+	//Update
+	public void update(Employee employee);
+	//Delete
+	public void deleteById(int id);
+	//Get All
+	public List<Employee> getAll();
+}
